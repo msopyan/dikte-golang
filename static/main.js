efd5a1d180c463a11438706dbeb775cf7770870b
faqih <faqih@bahasakita.co.id>
2018-11-13 16:29:59 +0700

    new commit

---
+++ static/main.js
@@ -0,0 +1,17 @@
+ <script type="text/javascript">
+    
+        var  nav = document.getElementById('nav');
+        
+        window.onscroll = function(){
+
+            if (window.pageYOffset >100) {
+
+                nav.style.background = "#3DA0DD";
+                nav.style.boxShadow = "0px 4px 2px #3DA0DD";
+            }
+            else{
+                nav.style.background = "transparent";
+                nav.style.boxShadow = "none";
+            }
+        }
+</script>
